af8a5e64c54bb7100ef0703f8180fbfc32a94370
Alexander Ott <66271487+AlexanderHott@users.noreply.github.com>
2024-01-08 18:17:04 -0800

    format authRouter file and add localOnly middleware check

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -2,7 +2,7 @@
   auth.js handles all of the authentication routes
 */
 const express = require("express");
-const app = express.Router();
+const authRouter = express.Router();
 
 // Authentication
 var GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
@@ -14,68 +14,82 @@ configPassport(passport);
 const User = require("../models/User");
 
 // here are the authentication routes
-app.use(passport.initialize());
-app.use(passport.session());
+authRouter.use(passport.initialize());
+authRouter.use(passport.session());
 
 // here is where we check on their logged in status
-app.use((req, res, next) => {
-    res.locals.title = "Peer Review App";
+authRouter.use((req, res, next) => {
+  res.locals.title = "Peer Review App";
+  res.locals.loggedIn = false;
+  if (req.isAuthenticated()) {
+    res.locals.user = req.user;
+    res.locals.loggedIn = true;
+  } else {
+    res.locals.user = {};
     res.locals.loggedIn = false;
-    if (req.isAuthenticated()) {
-      res.locals.user = req.user;
-      res.locals.loggedIn = true;
-    } else {
-      res.locals.user={};
-      res.locals.loggedIn = false;
-    }
-    next();
-  });
+  }
+  next();
+});
 
-app.get("/loginerror", function (req, res) {
-    res.locals.routeName = " loginerror";
-    res.render("loginerror", {}); 
+authRouter.get("/loginerror", function (req, res) {
+  res.locals.routeName = " loginerror";
+  res.render("loginerror", {});
+});
+
+authRouter.get("/login", function (req, res) {
+  res.locals.routeName = " login";
+  res.locals.allow_local_login = process.env.ALLOW_LOCAL_LOGIN;
+  console.log("in /login: allow_local_login = " + res.locals.allow_local_login);
+  res.render("login", {});
+});
+
+// route for logging out
+authRouter.get("/logout", function (req, res) {
+  req.session.destroy((error) => {
+    console.log("Error in destroying session: " + error);
   });
-  
-  app.get("/login", function (req, res) {
-    res.locals.routeName = " login";
-    res.locals.allow_local_login = process.env.ALLOW_LOCAL_LOGIN;
-    console.log('in /login: allow_local_login = '+res.locals.allow_local_login);
-    res.render("login", {});
-  });
-  
-  // route for logging out
-  app.get("/logout", function (req, res) {
-    req.session.destroy((error) => {
-      console.log("Error in destroying session: " + error);
-    });
-    req.logout();
-    res.redirect("/");
-  });
-  
-  // =====================================
-  // GOOGLE ROUTES =======================
-  // =====================================
-  // send to google to do the authentication
-  // profile gets us their basic information including their name
-  // email gets their emails
-  app.get("/auth/google", passport.authenticate("google", {scope: ["profile", "email"]}));
-  
-  app.get(
-    "/login/authorized",
-    passport.authenticate("google", {
-      successRedirect: "/lrec",
-      failureRedirect: "/loginerror",
-    })
-  );
-  
-  app.get("/login/local", async (req, res, next) => {
-    res.locals.routeName = " login";
-    res.render("localLogin");
-  });
-  
-  app.post("/auth/local/register", function (req, res, next) {
-    console.log("registering user");
-    User.register(new User({googleemail: req.body.username, googlename: req.body.name}), req.body.password, function (err) {
+  req.logout();
+  res.redirect("/");
+});
+
+// =====================================
+// GOOGLE ROUTES =======================
+// =====================================
+// send to google to do the authentication
+// profile gets us their basic information including their name
+// email gets their emails
+authRouter.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] }),
+);
+
+authRouter.get(
+  "/login/authorized",
+  passport.authenticate("google", {
+    successRedirect: "/lrec",
+    failureRedirect: "/loginerror",
+  }),
+);
+
+function localOnly(req, res, next) {
+  if (process.env.NODE_ENV === "development") {
+    next();
+  } else {
+    res.status(404).send("Not Found");
+  }
+}
+
+authRouter.get("/login/local", localOnly, async (req, res, next) => {
+  res.locals.routeName = " login";
+  res.render("localLogin");
+});
+
+authRouter.post("/auth/local/register", localOnly, function (req, res, next) {
+  console.log("registering user");
+  User.register(
+    new User({ googleemail: req.body.username, googlename: req.body.name }),
+    req.body.password,
+    function (err) {
       if (err) {
         console.log("error while user register!", err);
         // try {
@@ -86,15 +100,21 @@ app.get("/loginerror", function (req, res) {
         // }
         return next(err);
       }
-  
-      console.log("user registered!");
-  
-      res.redirect("/login/local");
-    });
-  });
-  
-  app.post("/auth/local/login", passport.authenticate("local", {failureRedirect: "/loginerror"}), function (req, res) {
-    res.redirect("/lrec");
-  });
 
-module.exports = app;
+      console.log("user registered!");
+
+      res.redirect("/login/local");
+    },
+  );
+});
+
+authRouter.post(
+  "/auth/local/login",
+  localOnly,
+  passport.authenticate("local", { failureRedirect: "/loginerror" }),
+  function (req, res) {
+    res.redirect("/lrec");
+  },
+);
+
+module.exports = authRouter;

6ecdd6b06ad8e765c653da924e0cba8a4ea85151
tim <tjhickey@brandeis.edu>
2024-08-20 13:49:39 +0900

    fixed cas error that Mac ignores

--- models/PsetProblem.js
+++ models/PsetProblem.js
@@ -6,7 +6,7 @@
 */
 const mongoose = require( 'mongoose' );
 const Schema = mongoose.Schema;
-const ObjectId = mongoose.Schema.Types.ObjectId;
+const ObjectId = mongoose.Schema.Types.ObjectId; 
 
 var psetProblemSchema = Schema( {
   courseId: {type:ObjectId,index:true,ref:'Course'},
